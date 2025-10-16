4cd3cdd664f5d6936ff833ec3248b4889314ad7c
imonitie-ochie <ochieimonite@gmail.com>
2025-10-16 11:14:34 +0100

    fixed

--- back_end/Routing/commentsRoutes.js
+++ back_end/Routing/commentsRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const commentController = require("../controllers/commentController");
+const {addComment ,GetCommentsByProduct} =  require('../Controler/commentController');
+
+
 
 // Add a new comment
-router.post("/add", commentController.addComment);
+router.post("/add", addComment);
 
 // Get all comments for a product by product name
-router.get("/:productName", commentController.getCommentsByProduct);
+router.get("/:productName", GetCommentsByProduct);
 
 module.exports = router;
